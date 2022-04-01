8071aeb38a30d1f8b2bc00d991bfeb8edc45876c
Xin-Shu <xins@tcd.ie>
2022-04-01 13:50:10 +0100

    DNN able to run

--- scripts/dnnModule.py
+++ scripts/dnnModule.py
@@ -1,34 +1,109 @@
+import os
 import sys
-import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from datetime import date
+import numpy as np
+import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 inputFilePath = '../dataset/input.csv'
 outputFilePath = '../dataset/output.csv'
+modelFilePath = '../dataset/trainedCNNModel.h5'
+
+inputDataSize, numOfClasses = 81, 10
+lengthData = 100000
 
 
-def extractCSV(inputPath, outputPath):
-    inputArr, outputArr = [], []
+def extractCSV(inputPath, outputPath, lenData):
+    global inputDataSize
+    inputArr, outputArr = np.zeros((lenData, 9, 9, 1)), np.zeros((lenData, inputDataSize))
     inputFile = pd.read_csv(inputPath)
     outputFile = pd.read_csv(outputPath)
 
     count = 0
-    while count <= 100:
-        x_in = inputFile.iloc[count, 1:]
-        y_out = outputFile.iloc[count, 1:]
-
-        inputArr.append(x_in)
-        outputArr.append(y_out)
+    while count < lenData:
+        x_in = np.array(inputFile.iloc[count, 1:]).reshape((9, 9, 1))
+        y_out = np.array(outputFile.iloc[count, 1:])
+        inputArr[count, :] = x_in
+        outputArr[count, :] = y_out
         count += 1
     return inputArr, outputArr
 
 
-def dnnLearning(feature, target):
-    pass
+def dnnArchitecture(inputSize, numClasses):
+
+    inputSize_ = inputSize
+
+    model = keras.Sequential()
+
+    model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(9, 9, 1)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(128, kernel_size=(1, 1), activation='relu', padding='same'))
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(81 * 9))
+    model.add(layers.Reshape((-1, 9)))
+    model.add(layers.Activation('softmax'))
+
+    return model
+
+
+def trainModel(modelPath, trainData, testTarget, inputSize, numClasses):
+
+    model = dnnArchitecture(inputSize, numClasses)
+    adam = keras.optimizers.Adam(lr=0.001)
+
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=adam)
+    model.summary()
+    callbacks = [
+        keras.callbacks.ModelCheckpoint(modelPath, save_best_only=True)
+    ]
+    epochs = 10
+    history = model.fit(
+        trainData, testTarget, validation_split=0.2,
+        batch_size=32, epochs=epochs, callbacks=callbacks
+    )
+
+    # list all data in history
+    print(history.history.keys())
+
+    # summarize history for accuracy
+    # fig1 = plt.figure(figsize=(8, 6))
+    # plt.title("Training history - Accuracy", fontsize=20)
+    # plt.plot(history.history['acc'])
+    # plt.plot(history.history['val_acc'])
+    # plt.ylabel('accuracy')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'test'], loc='upper left')
+    # plt.savefig(f'../dataset/val_acc_plot.png', )
+
+    # summarize history for loss
+    fig2 = plt.figure(figsize=(8, 6))
+    plt.title("Training history - Loss", fontsize=20)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper right')
+    plt.savefig(f'../dataset/val_loss_plot.png', )
+    plt.show()
+
+
+def main(args):
+    keras.backend.clear_session()
+    os.environ['DML_VISIBLE_DEVICES'] = '0'
+
+    trainSet, targetSet = extractCSV(inputFilePath, outputFilePath, lengthData)
+    # trainSet = keras.utils.to_categorical(trainSet, numOfClasses)
+    # targetSet = keras.utils.to_categorical(targetSet, numOfClasses)
+
+    trainModel(modelFilePath, trainSet, targetSet, inputDataSize, numOfClasses)
 
 
 if __name__ == '__main__':
+    main(sys.argv)
 
-    x, y = extractCSV(inputFilePath, outputFilePath)
---
+++ scripts/get_data.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def get_data(file):
+
+    data = pd.read_csv(file)
+
+    feat_raw = data.iloc[:, 0]
+    label_raw = data.iloc[:, 1]
+
+    feat = []
+    label = []
+
+    for i in feat_raw[:100]:
+
+        x = np.array([int(j) for j in i]).reshape((9,9,1))
+        feat.append(x)
+
+    feat = np.array(feat)
+    feat = feat/9
+    feat -= .5
+
+    for i in label_raw[:100]:
+
+        x = np.array([int(j) for j in i]).reshape((81,1)) - 1
+        label.append(x)
+
+    label = np.array(label)
+
+    del(feat_raw)
+    del(label_raw)
+
+    x_train, x_test, y_train, y_test = train_test_split(feat, label, test_size=0.2, random_state=42)
+
+    print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+
+    return x_train, x_test, y_train, y_test
+
+
+if __name__ == '__main__':
+    get_data('../dataset/sudoku.csv')
